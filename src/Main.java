e6c136a9f44b037091e50f659944767ebad3d15f
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-11-18 21:45:24 -0500

    -

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        MyLinkedList linkedList = new MyLinkedList(3);
+
+        linkedList.getHead();
+        linkedList.getTail();
+        linkedList.getLength();
+
+        linkedList.printList();
+
+
+
+    }
+}
